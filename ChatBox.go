39bade5121e84aec34e35c01d58a516f83e411f8
Jonathan Mutune <tonymutune30@gmail.com>
2023-10-27 10:46:53 +0300

    first commit

---
+++ ChatBox.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+type bot interface {
+	getGreeting() string
+}
+type englishbot struct{}
+type spanishbot struct{}
+
+func main() {
+
+	eb := englishbot{}
+	sb := spanishbot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+func (englishbot) getGreeting() string {
+	return "Hi There"
+}
+
+func (spanishbot) getGreeting() string {
+	return "Hola!"
+}
